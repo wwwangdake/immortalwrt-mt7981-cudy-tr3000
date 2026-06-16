1a205c642a0c89553b1450487f4ddf130f7b5efe
小喵崽 <asrtroh@gmail.com>
2026-06-16 12:39:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,20 +10,84 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+set -e
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo "============================================================"
+echo " DIY PART1: Add extra packages before feeds update"
+echo "============================================================"
 
-# Copy custom local packages into OpenWrt tree so they are available during build
+mkdir -p package
+
+# ============================================================
+# Keep local luci-compat package if exists
+# ============================================================
+
 if [ -d "$GITHUB_WORKSPACE/package/luci-compat-keep" ]; then
-  mkdir -p package
+  echo "Copy local luci-compat-keep"
+  rm -rf package/luci-compat-keep
   cp -r "$GITHUB_WORKSPACE/package/luci-compat-keep" package/
 fi
 
-git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
-git clone https://github.com/eamonxg/luci-app-aurora-config package/luci-app-aurora-config
-git clone https://github.com/timsaya/luci-app-bandix package/luci-app-bandix
-git clone https://github.com/timsaya/openwrt-bandix package/openwrt-bandix
+# ============================================================
+# Clean old extra packages
+# ============================================================
+
+rm -rf package/passwall
+rm -rf package/passwall-packages
+rm -rf package/nikki
+rm -rf package/luci-theme-aurora
+rm -rf package/luci-app-aurora-config
+rm -rf package/luci-theme-argon
+rm -rf package/luci-app-argon-config
+rm -rf package/luci-app-bandix
+rm -rf package/openwrt-bandix
+
+# ============================================================
+# PassWall
+# ============================================================
+
+echo "Clone PassWall"
+
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+
+# ============================================================
+# Nikki
+# ============================================================
+
+echo "Clone Nikki"
+
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.git package/nikki
+
+# ============================================================
+# LuCI Themes: Aurora / Argon
+# ============================================================
+
+echo "Clone Aurora theme"
+
+git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora.git package/luci-theme-aurora
+git clone --depth=1 https://github.com/eamonxg/luci-app-aurora-config.git package/luci-app-aurora-config
+
+echo "Clone Argon theme"
+
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+
+# ============================================================
+# Bandix
+# ============================================================
+
+echo "Clone Bandix"
+
+git clone --depth=1 https://github.com/timsaya/luci-app-bandix.git package/luci-app-bandix
+git clone --depth=1 https://github.com/timsaya/openwrt-bandix.git package/openwrt-bandix
+
+# ============================================================
+# Fix common package conflicts
+# ============================================================
+
+find package -name ".git" -type d -prune -exec rm -rf {} +
+
+echo "============================================================"
+echo " DIY PART1 done"
+echo "============================================================"
